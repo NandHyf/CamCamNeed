78a267f43bf88839ca80944b85f71fc30b95da94
NandHyf <heyufei@techlake.club>
2024-06-13 11:45:55 +0800

    修改完成!(也许可以tag v1.0了)

--- Core/Plot.py
+++ Core/Plot.py
@@ -21,18 +21,28 @@ class Charts():
         return kl, vl
 
 
-    def Bar(self):
+    def Bar(self, gridTicks=[]):
+        gt = gridTicks
         Charts.SortDict(self)
 
         plt.style.use('_mpl-gallery')
 
         fig, ax = plt.subplots(figsize=(11, 4), layout='constrained')
-        rects = ax.bar(self.x, self.y, width=1, edgecolor="white", linewidth=1, tick_label=self.x)
+        if gt == []:
+            rects = ax.bar(self.x, self.y, width=1, edgecolor="white", linewidth=1, tick_label=self.x)
+        
+        elif gt != []:
+            rects = ax.bar(self.x, self.y, width=1, edgecolor="white", linewidth=1)
+            
         ax.bar_label(rects, padding=0.2)
+        ax.set_xticks(gt)
+
         ax.set_xlabel('35 mm Equivalent Focal Length')
         ax.set_ylabel('Number of Photos')
         
         plt.grid(False)
+        plt.yticks([])
+        ax.xaxis.grid(True)
         plt.show()
 
     def Scatter(self):
--- Core/Start.py
+++ Core/Start.py
@@ -1,6 +1,7 @@
 import Extract
 import Plot
+from Model import PreConfig
 
 if __name__ == "__main__":
     fls_dict = Extract.GetFls(Extract.GetFolders())
-    Plot.Charts(fls_dict).Bar()
+    Plot.Charts(fls_dict).Bar(PreConfig.famousFLs)
